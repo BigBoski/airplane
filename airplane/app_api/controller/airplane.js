be57f3dde3921e00ba7fa2cc73d16c79898ab680
Boski <bonner_bo@yahoo.com>
2016-10-31 22:46:48 +0000

    some changes for homework

--- airplane/app_api/controller/airplane.js
+++ airplane/app_api/controller/airplane.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 var ClimbTable = mongoose.model('ClimbTable');
-
-
+var Flaps5Table = mongoose.model('Flaps5Table');
+var Flaps10Table = mongoose.model('Flaps10Table');
+var Flaps15Table = mongoose.model('Flaps15Table');
+var LandingTable = mongoose.model('LandingTable');
+var airplanectrl = require('../../app_server/controller/airplane');
 //utility method for the module
 var sendJSONresponse = function(res, status, content)
 {
@@ -9,11 +12,120 @@ var sendJSONresponse = function(res, status, content)
     res.json(content);
 }
 
+
 /* GET a location by the id */
 module.exports.airplaneReadOne = function(req, res) {
   console.log('Finding location details', req.params);
   if (req.params && req.params.locationid) {
     ClimbTable
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
+};
+
+module.exports.airplaneReadTwo = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Flaps5Table
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
+};
+
+module.exports.airplaneReadThree = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Flaps10Table
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
+};
+
+module.exports.airplaneReadFour = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Flaps15Table
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
+};
+
+module.exports.airplaneReadFive = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    LandingTable
       .findById(req.params.locationid)
       .exec(function(err, location) {
         if (!location) {
--- airplane/app_api/models/Flaps.js
+++ airplane/app_api/models/Flaps.js
@@ -1,9 +1,51 @@
 var mongoose = require('mongoose');
 
-var ClimbTable = new mongoose.Schema({
+var ClimbTable = new mongoose.Schema ({
 	weight: Number,
-	vfri5: Number,
-	vfri10: Number,
-	vfri15: Number,
-	vclmb: Number
+	VFRIFlap5: Number,
+	VFRIFlap10: Number,
+	VFRIFlap15: Number,
+	VCLMB: Number
 });
+
+var flaps5Table = new mongoose.Schema({
+  temperature: String,
+              weight:Number,
+              altitude:{Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number},
+              Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number},
+              Number:{Vr:Number,V1:Number}}
+});
+
+var flaps10Table = new mongoose.Schema({
+  temperature: String,
+              weight:Number,
+              altitude:{Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number},
+              Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number},
+              Number:{Vr:Number,V1:Number}}
+});
+
+var flaps15Table = new mongoose.Schema({
+  temperature: String,
+              weight:Number,
+              altitude:{Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number},
+              Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number}, Number:{Vr:Number,V1:Number},
+              Number:{Vr:Number,V1:Number}}
+});
+
+var landingTable = mongoose.Schema ({
+	flap: Number,
+	weight: Number,
+	Vapp: Number,
+	Vref: Number,
+	Vga: Number
+});
+
+mongoose.model('ClimbTable', ClimbTable, 'climbTable')
+
+mongoose.model('Flaps5Table', flaps5Table, 'flaps5Table')
+
+mongoose.model('Flaps10Table', flaps10Table, 'flaps10Table')
+
+mongoose.model('Flaps15Table', flaps15Table, 'flaps15Table')
+
+mongoose.model('LandingTable', landingTable, 'landingTable')
